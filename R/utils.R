0c4ec6199ea7173381700d14b723ea033c5a47b2
whopper-sculder <wjhopper510@gmail.com>
2015-08-18 08:40:36 -0400

    helper function for checking parameter bounds and required parameters

---
+++ R/utils.R
@@ -0,0 +1,19 @@
+#' @export
+paramBounds <- function(p, lower= 0, upper = 1) {
+  if (any(p[names(p) %in% c("ER","LR","TR","F1","theta","space")] > upper, na.rm = TRUE) ||
+      any(p[names(p) %in% c("ER","LR","TR","F1","lambda","theta","space")] < lower, na.rm = TRUE)) {
+    return(FALSE)
+  } else {
+    return(TRUE)
+  }
+}
+
+#' @export
+checkRequiredParams <- function(m,fcn) {
+  reqParams <- c(names(formals(m$fcn)$free), names(formals(m$fcn)$fix))
+  reqParams <- reqParams[!reqParams %in%  c("","Time","Tmin","Tmax","lambda")]
+  givenParams <- names(c(m$free,m$fix))
+  if (!all( reqParams[reqParams != ""]  %in% givenParams)) {
+    stop(paste(reqParams[!reqParams %in% givenParams], " not specified in model,check model input list"))
+  }
+}
