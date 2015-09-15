97bd735b0c785bc37510b4a7ae689d702c583276
whopper-sculder <wjhopper510@gmail.com>
2015-09-15 09:49:51 -0400

    freeRecall now returns all RTs, even to unrecoverable and timed out items. Makes for better ability to explore and diagnose model

--- R/recall.R
+++ R/recall.R
@@ -45,9 +45,10 @@ cuedRecall <- function(mem, thresh, space=NULL,Tmin=NULL,Tmax=NULL,
 #' @param Time
 #' @param lambda
 #'
-#' @return If time parameters are used, list of 3 matrices representing
-#'  accuracy, reaction time, and serial order. If time parameters are not used, list of 2
-#'  matrices representing accuracy and serial order.
+#' @return If time parameters are used, list of 4 matrices representing
+#'  accuracy, recoverability, reaction time, and serial order.
+#'  If time parameters are not used, returns list of 2 matrices representing
+#'  accuracy and serial order.
 #' @export
 #'
 freeRecall <- function(mem, thresh, space=NULL,Tmin=NULL,Tmax=NULL,
@@ -58,25 +59,35 @@ freeRecall <- function(mem, thresh, space=NULL,Tmin=NULL,Tmax=NULL,
     return(list(Acc=recalled, order=serialOrder))
 
   } else {
-    RT <- serialOrder <- matrix(NA, nrow = nrow(mem), ncol = ncol(mem))
-    recalled <- matrix(FALSE, nrow = nrow(RT), ncol = ncol(RT))
-    recoverable <- recalled
+
+    # Preallocate return structures
+    RT <- matrix(NA, nrow = nrow(mem), ncol = ncol(mem))
+    serialOrder <- matrix(NA, nrow = nrow(mem), ncol = ncol(mem))
+    recalled <- matrix(FALSE, nrow = nrow(mem), ncol = ncol(mem))
+    recoverable <- matrix(FALSE, nrow =nrow(mem), ncol = ncol(mem))
+
     for (i in 1:nrow(RT)) {
+
+      # Find search order, and reverse indices
       ord <- order(mem[i,],decreasing=TRUE)
       reverseOrd <- order(ord)
-      CRT <- cumsum(Tmin + (Tmax-Tmin)*exp(-lambda*abs(mem[i,]-thresh[i,]))[ord])
-      acc <- CRT < Time & (mem[i,ord]  >= thresh[i,ord])
-      rec <- mem[i,ord]  >= thresh[i,ord]
-      if (any(acc)) {
-        RT[i,ord[acc]] <- c(CRT[acc][1],diff(CRT[acc]))
-      }
+
+      # Calculate RT and accuracy
+      rt <- Tmin + (Tmax-Tmin)*exp(-lambda*abs(mem[i,]-thresh[i,]))
+      rec <- mem[i,]  >= thresh[i,][ord]
+      acc <- cumsum(rt[ord]) < Time & rec
+
+      # Fill in the output structures
+      # In simulation order, not search order!!!!
+      RT[i,] <- rt
       recalled[i,] <- acc[reverseOrd]
       recoverable[i,] <- rec[reverseOrd]
       serialOrder[i,] <- ord
-    }
-  }
+    } # close for
+  } # close if switch
 
-  return(list(Acc=recalled,RTrounded=RT,order=serialOrder,recoverable = recoverable))
+  return(list(Acc=recalled,RTrounded=RT,
+              order=serialOrder,recoverable = recoverable))
 
   # tests
   # max(mem[i,])==mem[i,ord[1]] & min(mem[i,])==mem[i,ord[15]]
@@ -84,4 +95,4 @@ freeRecall <- function(mem, thresh, space=NULL,Tmin=NULL,Tmax=NULL,
   # all((mem[i,] >= thresh[i,])==recalled[i,])
   # all(!is.na(RT[i,]) ==recalled[i,] & is.na(RT[i,]) == !recalled[i,])
 
-}
+} # Close freeRecall
