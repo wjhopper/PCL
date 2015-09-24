af8601d3a9e4b950fe60db3577f74ae5a1ae7e45
whopper-cemnl <wjhopper510@gmail.com>
2015-09-24 15:53:24 -0400

    Introduced new test_beta and study_beta functions.
    These will operate on continous (double typed) memory and threshold matrices, and updat
    them with draws from the beta distribution. The beta is parameterized with
    its two shape parameters, but are constrained to have same mean and sd as a binomial distrution, where the
    binomial is parameterized by the number of features/threshold and relevant model parameters like LR, TR and FR.
    
    study() and test() are now wrappers for the function you really want (*_binomial or *beta) and will
    call either one based on the type of data in the matrix. These should be considered a deprecated compatability
    layer to avoid breaking old models.

--- R/memories.R
+++ R/memories.R
@@ -1,4 +1,128 @@
-#' study
+#' @title Study
+#' @description Effects of studying on un-encoded features
+#' @param ... Arugments passed to approriate functions based on type-checking. See details.
+#' @details test will attempt to type-check the mem matrix and call the
+#' appropriate function (study_beta for doubles,study_binomial for integers).
+#' @export
+#' @import whoppeR
+#' @name study
+study <- function(...) {
+  if (typeof(list(...)$mem) == "double") {
+    return(study_beta(...))
+  } else if (typeof(list(...)$mem) == "integer") {
+    return(study_binomial(...))
+  } else {
+    stop("unable to determine data type in matrix.
+         Call study_beta or study_binomial directly")
+  }
+}
+
+
+#' @title Test
+#' @description Effects of Testing on un-encoded features and thresholds
+#' @param ... Arugments passed to approriate functions based on type-checking. See details.
+#' @details \code{test} is a conveince function, and will attempt to type-check
+#' the code{mem} matrix to call the appropriate function (test_beta for doubles,
+#' test_binomial for integers).
+#'
+#' \code{test_beta} and \code{test_binomial} can (and should) be called directly though,
+#' especially when fitting models and computational time matters. See Functions section
+#' for more details.
+#' @import whoppeR
+#' @export
+#' @name test
+test <- function(...) {
+  if (typeof(list(...)$mem) == "double") {
+    return(test_beta(...))
+  } else if (typeof(list(...)$mem) == "integer") {
+    return(test_binomial(...))
+  } else {
+    stop("unable to determine data type in matrix.
+         Call test_beta or test_binomial directly")
+  }
+}
+#' study beta
+#' @describeIn study study_beta is for continous data
+#' @param mem Matrix of memory strengths.
+#' @param nFeatures Number of features the memory for each item has
+#' @param LR Learning Rate. Gives the probability of learning a new features
+#'  through practice
+#' @param FR Forgetting Rate. Gives the probability of forgetting a feature
+#'
+#' @return Matrix of memory strengths updated after restudying
+#' @export
+study_beta <- function(mem, nFeatures, LR, FR = NULL) {
+
+  mxn <- prod(dim(mem))
+  mem[nFeatures - mem  < 1] <- nFeatures-1
+  toGo <- nFeatures - mem
+  binomVAR <- toGo*LR*(1-LR)
+  binomM <- toGo*LR
+  beta_pars <- betaABfromMeanSD(binomM/toGo, sqrt(binomVAR/(toGo^2)))
+  strengths <- mem + (rbeta(mxn, beta_pars$a, beta_pars$b) * toGo)
+
+  if (!is.null(FR)) {
+    strengths <- ceiling(strengths)
+    binomVAR <-  strengths*FR*(1-FR)
+    binomM <-  strengths*FR
+    beta_pars <- betaABfromMeanSD(binomM/strengths, sqrt(binomVAR/(strengths^2)))
+    strengths <- strengths - (rbeta(mxn, beta_pars$a, beta_pars$b) * strengths)
+  }
+
+  return(strengths)
+}
+
+#' @describeIn test \code{test_beta} is designed to be used with a matrix of
+#' continous feature and threhsold values (represented as doubles) drawn from
+#' a beta distribution
+#' @inheritParams study_beta
+#' @param thresh Matrix of memory thresholds. Threshold is the number of active features
+#'  needed to support pattern completetion
+#' @param acc Logical matrix of performance for previous test
+#' @param TR Threshold Reduction rate. Describes the probability of removing the need
+#'  for a remembering a particular feature following successfull recall
+#'
+#' @return List of two matrices. mem is a matrix of memory strengths following succcesful
+#'  recall. thresh is a matrix of feature thresholds following succesful recall.
+#'
+#' @export
+test_beta <- function(mem, nFeatures, thresh, acc, LR, TR, FR=NULL) {
+
+  strengths <- mem #copy encoded features. Strengths will be updated and returned, not mem
+  theta  <- thresh #copy current thresholds. theta will be updated and returned, not thresh
+  mxn <- prod(dim(mem))
+  nCor = sum(acc)
+
+  # memory feature updating
+  strengths[nFeatures - strengths < 1] <- nFeatures-1
+  toGo <-nFeatures - strengths
+  binomVAR <- toGo*LR*(1-LR)
+  binomM <- toGo*LR
+  beta_pars <- betaABfromMeanSD(binomM/toGo, sqrt(binomVAR/(toGo^2)))
+  strengths[acc] <- mem[acc] + (rbeta(nCor, beta_pars$a, beta_pars$b) * toGo[acc])
+
+  # threshold updating
+  thresh[thresh < 1] <- 1
+  binomVAR <- thresh*TR*(1-TR)
+  binomM <-  thresh*TR
+  beta_pars <- betaABfromMeanSD(binomM/thresh, sqrt(binomVAR/thresh^2))
+  theta[acc] <- thresh[acc] - (rbeta(nCor, beta_pars$a[acc], beta_pars$b[acc])
+                               * thresh[acc])
+
+  if (!is.null(FR)) {
+    strengths[strengths < 1] <- 1
+    binomVAR <-  strengths*FR*(1-FR)
+    binomM <-  strengths*FR
+    beta_pars <- betaABfromMeanSD(binomM/strengths, sqrt(binomVAR/(strengths^2)))
+    strengths <- strengths - (rbeta(mxn, beta_pars$a, beta_pars$b) * strengths)
+  }
+
+  return(list(mem = strengths,thresh = theta))
+}
+
+
+#' study binomial
+#' @describeIn study study_binomial is for discrete features
 #'
 #' @param mem Matrix of memory strengths.
 #' @param nFeatures Number of features the memory for each item has
@@ -8,40 +132,37 @@
 #'
 #' @return Matrix of memory strengths updated after restudying
 #' @export
-study <- function(mem, nFeatures, LR, FR = NULL) {
+study_binomial <- function(mem, nFeatures, LR, FR = NULL) {
 
   mxn <- prod(dim(mem))
-  strengths <- mem + rbinom(mxn, as.integer(nFeatures - mem), LR)
+  strengths <- mem + rbinom(mxn, nFeatures - mem, LR)
   if (!is.null(FR)) {
-    strengths <- strengths - rbinom(mxn, as.integer(strengths), FR)
+    strengths <- strengths - rbinom(mxn, strengths, FR)
   }
 
   return(strengths)
 }
 
-#' test
-#'
-#' @inheritParams study
+#' @describeIn test \code{test_binomial} is designed to be used with a matrix of
+#' discrete feature and threshold values (represented as integers) drawn from
+#' a binomial distribution
+#' @inheritParams test_beta
 #' @param thresh Matrix of memory thresholds. Threshold is the number of active features
-#'  needed to support pattern completetion
+#' needed to support pattern completetion. Must hold integer values.
 #' @param acc Logical matrix of performance for previous test
 #' @param TR Threshold Reduction rate. Describes the probability of removing the need
-#'  for a remembering a particular feature following successfull recall
-#'
-#' @return List of two matrices. mem is a matrix of memory strengths following succcesful
-#'  recall. thresh is a mastrix of feature thresholds following succesful recall.
-#'
+#' for a remembering a particular feature following successfull recall
 #' @export
-test <- function(mem, nFeatures, thresh, acc, LR, TR, FR=NULL) {
+test_binomial <- function(mem, nFeatures, thresh, acc, LR, TR, FR=NULL) {
 
   strengths <- mem #copy strengths and thresholds from practice test
   theta  <- thresh
   mxn <- prod(dim(mem))
   nCor = sum(acc)
-  strengths[acc] <- mem[acc] + rbinom(nCor,as.integer(nFeatures - mem[acc]), LR)
-  theta[acc] <- thresh[acc] - rbinom(nCor, as.integer(thresh[acc]), TR)
+  strengths[acc] <- mem[acc] + rbinom(nCor, nFeatures - mem[acc], LR)
+  theta[acc] <- thresh[acc] - rbinom(nCor, thresh[acc], TR)
   if (!is.null(FR)) {
-    strengths <- strengths - rbinom(mxn, as.integer(strengths), FR)
+    strengths <- strengths - rbinom(mxn, strengths, FR)
   }
 
   return(list(mem = strengths,thresh = theta))
--- tests/testthat/test_memories.R
+++ tests/testthat/test_memories.R
@@ -1,6 +1,6 @@
-library(PCL)
 context("Testing the arrays produced by test() and study()")
 
+# Parameters
 min <- 0
 max <- 100
 tmin = .5
@@ -10,19 +10,31 @@ time = 90
 lr = .15
 tr = .1
 
+# Memory data
+mem <-structure(c(56L, 55L, 61L, 51L, 99L, 51L, 59L, 50L, 51L, 56L,
+                  50L, 53L, 54L, 57L, 58L),
+                .Dim = c(3L, 5L))
+thresh <- structure(c(56L, 46L, 46L, 50L, 98L, 02L, 43L, 52L, 56L, 49L,
+                      51L, 48L, 47L, 51L, 54L),
+                    .Dim = c(3L, 5L))
+
+mem_dec <- structure(c(0.615723615968426, 0.589972537391328, 0.467912515995964,
+                       0.522620685556245, 0.607614257503363, 0.980055358207064, 0.514484390626524,
+                       0.576597597433024, 0.585887959352946, 0.580708899736991, 0.658038268679618,
+                       0.562303542618935, 0.646057922583702, 0.002138009703025, 0.628875291380699
+), .Dim = c(3L, 5L)) * max
+
+thresh_dec <- structure(c(0.482994295176146, 0.554661452924281, 0.992227641234684,
+                          0.556897972078241, 0.485085693043906, 0.558457860883398, 0.454974711341695,
+                          0.488205791059824, 0.529774619827727, 0.593478434672639, 0.449333432257201,
+                          0.484600716134854, 0.0365049940911, 0.442111441874386, 0.497987068879674
+), .Dim = c(3L, 5L)) * max
 
 test_that("(Binomial) All strengths between the min and max features", {
 
-  mem <-structure(c(56L, 55L, 61L, 51L, 99L, 51L, 59L, 50L, 51L, 56L,
-                    50L, 53L, 54L, 57L, 58L),
-                  .Dim = c(3L, 5L))
-  thresh <- structure(c(56L, 46L, 46L, 50L, 98L, 02L, 43L, 52L, 56L, 49L,
-                        51L, 48L, 47L, 51L, 54L),
-                      .Dim = c(3L, 5L))
-
-  result <- freeRecall(mem, thresh, Tmin=tmin,Tmax=tmax,lambda= lambda,Time=time)
-  restudyStrengths <- study(mem, nFeatures=max, LR = lr)
-  testStrengths <- test(mem,  nFeatures=max, thresh = thresh, acc = result$Acc,
+  result <- freeRecall(mem , thresh, Tmin=tmin,Tmax=tmax,lambda= lambda,Time=time)
+  restudyStrengths <- study_binomial(mem=mem, nFeatures=max, LR = lr)
+  testStrengths <- test_binomial(mem=mem,  nFeatures=max, thresh = thresh, acc = result$Acc,
                         LR = lr, TR = tr)
 
   expect_true(all(restudyStrengths <= max))
@@ -39,30 +51,51 @@ test_that("(Binomial) All strengths between the min and max features", {
 test_that("(Beta) All strengths between the min and max features", {
 
 
-  mem <- structure(c(0.615723615968426, 0.589972537391328, 0.467912515995964,
-              0.522620685556245, 0.607614257503363, 0.997055358207064, 0.514484390626524,
-              0.576597597433024, 0.585887959352946, 0.580708899736991, 0.658038268679618,
-              0.562303542618935, 0.646057922583702, 0.002138009703025, 0.628875291380699
-  ), .Dim = c(3L, 5L)) * max
-
-  thresh <- structure(c(0.482994295176146, 0.554661452924281, 0.992227641234684,
-              0.556897972078241, 0.485085693043906, 0.558457860883398, 0.454974711341695,
-              0.488205791059824, 0.529774619827727, 0.593478434672639, 0.449333432257201,
-              0.484600716134854, 0.00365049940911, 0.442111441874386, 0.497987068879674
-  ), .Dim = c(3L, 5L)) * max
-
-  result <- freeRecall(mem, thresh, Tmin=tmin,Tmax=tmax,lambda= lambda,Time=time)
-  restudyStrengths <- study(mem, nFeatures=max, LR = lr)
-  testStrengths <- test(mem,  nFeatures=max, thresh = thresh, acc = result$Acc,
-                        LR = lr, TR = tr)
+  result <- freeRecall(mem_dec, thresh_dec, Tmin=tmin,Tmax=tmax,
+                       lambda= lambda,Time=time)
+  restudyStrengths <- study_beta(mem_dec, nFeatures=max, LR = lr)
+  testStrengths <- test_beta(mem_dec,  nFeatures=max, thresh = thresh_dec,
+                             acc = result$Acc, LR = lr, TR = tr)
 
   expect_true(all(restudyStrengths <= max))
   expect_true(all(restudyStrengths >= min))
   expect_true(all(testStrengths$mem <= max))
   expect_true(all(testStrengths$mem >= min))
   expect_true(all(testStrengths$thresh <= max))
+  # The following test will fail stochastically on item #13...
   expect_true(all(testStrengths$thresh >= min))
-  expect_true(all(restudyStrengths >= mem))
-  expect_true(all(testStrengths$mem >= mem))
-  expect_true(all(testStrengths$thresh <= thresh))
+  expect_true(all(restudyStrengths >= mem_dec))
+  expect_true(all(testStrengths$mem >= mem_dec))
+  expect_true(all(testStrengths$thresh <= thresh_dec))
+})
+
+test_that("study() and test() can type check correctly" ,{
+
+  # continous data
+  result <- freeRecall(mem_dec, thresh_dec, Tmin=tmin,Tmax=tmax,
+                       lambda= lambda,Time=time)
+  restudyStrengths <- study(mem = mem_dec, nFeatures=max, LR = lr)
+  testStrengths <- test(mem = mem_dec,  nFeatures=max, thresh = thresh_dec,
+                        acc = result$Acc, LR = lr, TR = tr)
+  restudyStrengths_direct <- study_beta(mem = mem_dec, nFeatures=max, LR = lr)
+  testStrengths_direct <- test_beta(mem = mem_dec,  nFeatures=max, thresh = thresh_dec,
+                                    acc = result$Acc, LR = lr, TR = tr)
+  expect_identical(typeof(restudyStrengths),typeof(restudyStrengths_direct))
+  expect_identical(typeof(testStrengths$mem),typeof(testStrengths_direct$mem))
+  expect_identical(typeof(testStrengths$thresh),typeof(testStrengths_direct$thresh))
+
+  # discrete data
+  result <- freeRecall(mem, thresh, Tmin=tmin,Tmax=tmax,
+                       lambda= lambda,Time=time)
+  restudyStrengths <- study(mem = mem, nFeatures=max, LR = lr)
+  testStrengths <- test(mem = mem,  nFeatures=max, thresh = thresh,
+                        acc = result$Acc, LR = lr, TR = tr)
+  restudyStrengths_direct <- study_binomial(mem = mem, nFeatures=max, LR = lr)
+  testStrengths_direct <- test_binomial(mem = mem,  nFeatures=max, thresh = thresh,
+                                    acc = result$Acc, LR = lr, TR = tr)
+  expect_identical(typeof(restudyStrengths),typeof(restudyStrengths_direct))
+  expect_identical(typeof(testStrengths$mem),typeof(testStrengths_direct$mem))
+  expect_identical(typeof(testStrengths$thresh),typeof(testStrengths_direct$thresh))
+
+
 })
