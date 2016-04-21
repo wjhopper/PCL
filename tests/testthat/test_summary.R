e80e2e6e40016585f4add49c3e8452f1331ae26a
whopper-sculder <wjhopper510@gmail.com>
2016-04-21 12:35:44 -0400

    updated summary.PCR tests to reflect that the summaries have the correct/incorrect breakdown for each cue

--- tests/testthat/test_summary.R
+++ tests/testthat/test_summary.R
@@ -8,8 +8,13 @@ tested <- summary(beta_t_tested)
 control <- summary(beta_t_no_prac)
 
 test_that("Practice methods are correctly specified", {
-  expect_equal(init_study$practice, c("none","none"))
-  expect_equal(restudy$practice, c("study","none"))
-  expect_equal(tested$practice, c("test","none"))
-  expect_equal(control$practice, c("none","none"))
+  expect_equal(init_study$practice, c("none","none","none","none"))
+  expect_equal(restudy$practice, c("study","study","none","none"))
+  expect_equal(tested$practice, c("test","test","none","none"))
+  expect_equal(control$practice, c("none","none","none","none"))
+})
+
+test_that("Proportion sums to 1", {
+  acc <- tested$proportion[!is.na(tested$proportion)]
+  expect_equal(sum(acc), length(acc)/2)
 })
